a5aa4c8261e23c329ae46fa68cb098351e4fc43c
Pablito14 <pablo.rosales14@gmail.com>
2018-07-10 16:30:28 -0700

    starting the quesions in JS, linked them to the index.html

---
+++ js/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var q1, q2, q3, q4, q5;
+
+q1 = prompt("HEY, are you a stressed out developer?! Type y or n to answer!")
+if(q1.toLowerCase === "y"){
+    alert("Good, you should be. >:(");
+    console.log(q1);
+}else if(q1.toLowerCase === "n"){
+    alert("Shifu please teach me <3");
+    console.log(q1);
+}else{
+    alert("Its a yes or no question, bruh. Type y or n");
+}
+
+q2 = prompt("Do you want to bang your head onto the table ")
+if(q2.toLocaleLowerCase === "y"){
+    alert("Good, go ahead and start. Harder. BREAK THE TABLE. RIOT. REEEEEEEEEEEEEEE");
+    console.log(q2);
+}else if (q2.toLowerCase === "n"){
+    alert("You may not like it, but you are what peak performance looks like");
+    console.log(q2);
+
+}
+
+
+

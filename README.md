a562ddb82de5f0084bf1d71e551c558e2b8d8d41
Pablito14 <pablo.rosales14@gmail.com>
2018-07-10 18:20:55 -0700

    git status

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # c201l2
 Course 201, lab 2 (about me project)
+
+# This project is a yes or no game 
+
+## There are 5 questions
+
+### 
--- js/app.js
+++ js/app.js
@@ -8,22 +8,22 @@ q1 = prompt("HEY, are you a stressed out developer?! Type y or n to answer!")
 if(q1.toLowerCase() === "y")
 {
     alert("Good, you should be. >:(");
-    console.log(q1);
+    console.log("question 1: " , q1);
 }
 else if(q1.toLowerCase() === "n")
 {
     alert("Shifu please teach me <3");
-    console.log(q1);
+    console.log("question 1: " , q1);
 }
 else{ alert(wrong)};
 
 q2 = prompt("Do you want to bang your head onto the table ")
 if(q2.toLocaleLowerCase() === "y"){
     alert("Good, go ahead and start. Harder. BREAK THE TABLE. RIOT. REEEEEEEEEEEEEEE");
-    console.log(q2);
+    console.log("question 2: " , q2);
 }else if (q2.toLowerCase() === "n"){
     alert("You may not like it, but you are what peak performance looks like");
-    console.log(q2);
+    console.log("question 2: " , q2);
 } else{
     alert(wrong);
 }
@@ -31,26 +31,26 @@ if(q2.toLocaleLowerCase() === "y"){
 q3 = prompt("Are you tired?")
 if(q3.toLowerCase() === "y"){
     alert("same");
-    console.log(q3);
+    console.log("question 3: " , q3);
 }else if(q3.toLowerCase() === "n"){
     alert("You were responsible!");
-    console.log(q3);
+    console.log("question 3: " , q3);
 }else{ alert(wrong)};
 
 q4 = prompt("Are you hungry ")
 if(q4.toLowerCase() === "y"){
     alert("Go get some food.");
-    console.log(q4);
+    console.log("question 4: " , q4);
 }else if(q4.toLowerCase() === "n"){
     alert("Not eveyone can be that fortunate >:(");
-    console.log(q4);
+    console.log("question 4: " , q4);
 }else{ alert(wrong)};
 
 q5 = prompt("Do you ever feel like breaking down? Do you ever feel out of place? Like somehow you just don't belong and no one understands you? Do you ever wanna run away? Do you lock yourself in your room? With the radio on turned up so loud that no one hears you screaming")
 if(q5.toLowerCase() === "y"){
     alert("No you don't know what it's like when nothing feels all right. You don't know what it's like to be like me, to be hurt, to feel lost, to be left out in the dark, to be kicked when you're down, to feel like you've been pushed around, to be on the edge of breaking down, and no one's there to save you. No you don't know what it's like. Welcome to my life");
-    console.log(q5);
+    console.log("question 5: " , q5);
 }else if(q5.toLowerCase() === "n"){
     alert("GO LISTEN TO SIMPLE PLAN'S: WELCOME TO MY LIFE");
-    console.log(q5);
+    console.log("question 5: " , q5);
 }else{ alert(wrong)};
